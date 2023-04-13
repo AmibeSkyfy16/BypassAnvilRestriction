920bcd7459f7d165594fb60fdd81575c542bab96
skyfy16 <colin.viatte@gmail.com>
2023-04-13 08:42:49 +0200

    1.19.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,173 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val transitiveInclude: Configuration by configurations.creating
 
+plugins {
+    id("fabric-loom") version "1.1-SNAPSHOT"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
+    idea
+}
+
+base {
+    archivesName.set(properties["archives_name"].toString())
+    group = property("maven_group")!!
+    version = property("mod_version")!!
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.repsy.io/mvn/amibeskyfy16/repo")
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${properties["minecraft_version"]}")
+    mappings("net.fabricmc:yarn:${properties["yarn_mappings"]}:v2")
+
+    modImplementation("net.fabricmc:fabric-loader:${properties["loader_version"]}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${properties["fabric_version"]}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${properties["fabric_kotlin_version"]}")
+
+    transitiveInclude(implementation("ch.skyfy.tomlconfiglib:toml-config-lib:1.0.2")!!)
+
+    handleIncludes(project, transitiveInclude)
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.20")
+}
+
+tasks {
+
+    val javaVersion = JavaVersion.VERSION_17
+
+    processResources {
+        inputs.property("version", project.version)
+        filteringCharset = "UTF-8"
+        filesMatching("fabric.mod.json") {
+            expand(mutableMapOf("version" to project.version))
+        }
+    }
+
+    loom {
+        runs {
+            this.getByName("client") {
+                runDir = "testclient"
+
+                val file = File("preconfiguration/doneclient.txt")
+                if (!file.exists()) {
+                    println("copying to client")
+                    file.createNewFile()
+
+                    // Copy some default files to the test client
+                    copy {
+                        from("preconfiguration/prepared_client/.")
+                        into("testclient")
+                        include("options.txt") // options.txt with my favorite settings
+                    }
+
+                    // Copying the world to use
+                    copy {
+                        from("preconfiguration/worlds/.")
+                        include("testworld#1/**")
+                        into("testclient/saves")
+                    }
+
+                    // Copying useful mods
+                    copy {
+                        from("preconfiguration/mods/client/.", "preconfiguration/mods/both/.")
+                        include("*.jar")
+                        into("testclient/mods")
+                    }
+
+                }
+            }
+            this.getByName("server") {
+                runDir = "testserver"
+
+                val file = File("preconfiguration/doneserver.txt")
+                if (!file.exists()) {
+                    file.createNewFile()
+                    println("copying to server")
+
+                    // Copy some default files to the test server
+                    copy {
+                        from("preconfiguration/prepared_server/.")
+                        include("server.properties") // server.properties configured with usefully settings
+                        include("eula.txt") // Accepted eula
+                        into("testserver")
+                    }
+
+                    // Copying the world to use
+                    copy {
+                        from("preconfiguration/worlds/.")
+                        include("testworld#1/**")
+                        into("testserver")
+                    }
+
+                    // Copying useful mods
+                    copy {
+                        from("preconfiguration/mods/server/.", "preconfiguration/mods/both/.")
+                        include("*.jar")
+                        into("testserver/mods")
+                    }
+                }
+            }
+        }
+    }
+
+    java {
+        toolchain {
+//            languageVersion.set(JavaLanguageVersion.of(javaVersion.toString()))
+//            vendor.set(JvmVendorSpec.BELLSOFT)
+        }
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    named<Wrapper>("wrapper") {
+        gradleVersion = "8.1"
+        distributionType = Wrapper.DistributionType.BIN
+    }
+
+    named<Javadoc>("javadoc") {
+        options {
+            (this as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
+        }
+    }
+
+    named<Jar>("jar") {
+        from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } }
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+//        kotlinOptions.freeCompilerArgs += "-Xskip-prerelease-check" // Required by others project like SilkMC. Also add this to intellij setting under Compiler -> Kotlin Compiler -> Additional ...
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(javaVersion.toString().toInt())
+    }
+
+    named<Test>("test") {// https://stackoverflow.com/questions/40954017/gradle-how-to-get-output-from-test-stderr-stdout-into-console
+        useJUnitPlatform()
+
+        testLogging {
+            outputs.upToDateWhen { false } // When the build task is executed, stderr-stdout of test classes will be show
+            showStandardStreams = true
+        }
+    }
+
+    val copyJarToTestServer = register("copyJarToTestServer") {
+        println("copying jar to server")
+//        copyFile("build/libs/${project.properties["archives_name"]}-${project.properties["mod_version"]}.jar", project.property("testServerModsFolder") as String)
+//        copyFile("build/libs/${project.properties["archives_name"]}-${project.properties["mod_version"]}.jar", project.property("testClientModsFolder") as String)
+    }
+
+    build { doLast { copyJarToTestServer.get() } }
+
+}
+
+fun copyFile(src: String, dest: String) = copy { from(src);into(dest) }
+
 fun DependencyHandlerScope.includeTransitive(
     root: ResolvedDependency?,
     dependencies: Set<ResolvedDependency>,
@@ -34,82 +201,4 @@ fun DependencyHandlerScope.handleIncludes(project: Project, configuration: Confi
         project.configurations.getByName("modImplementation").resolvedConfiguration.firstLevelModuleDependencies
             .first { it.moduleGroup == "net.fabricmc" && it.moduleName == "fabric-language-kotlin" }
     )
-}
-
-plugins {
-    id("fabric-loom") version "1.1-SNAPSHOT"
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
-    idea
-}
-
-base {
-    archivesName.set(properties["archives_name"].toString())
-    group = property("maven_group")!!
-    version = property("mod_version")!!
-}
-
-repositories {
-    mavenCentral()
-    maven("https://repo.repsy.io/mvn/amibeskyfy16/repo")
-}
-
-dependencies {
-    minecraft("com.mojang:minecraft:${properties["minecraft_version"]}")
-    mappings("net.fabricmc:yarn:${properties["yarn_mappings"]}:v2")
-
-    modImplementation("net.fabricmc:fabric-loader:${properties["loader_version"]}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${properties["fabric_version"]}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:${properties["fabric_kotlin_version"]}")
-
-    transitiveInclude(implementation("ch.skyfy.tomlconfiglib:toml-config-lib:1.0.2")!!)
-
-    handleIncludes(project, transitiveInclude)
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
-}
-
-tasks {
-
-    val javaVersion = JavaVersion.VERSION_17
-
-    processResources {
-        inputs.property("version", project.version)
-        filteringCharset = "UTF-8"
-        filesMatching("fabric.mod.json") {
-            expand(mutableMapOf("version" to project.version))
-        }
-    }
-
-    java { withSourcesJar() }
-
-    named<Wrapper>("wrapper") {
-        gradleVersion = "7.6"
-        distributionType = Wrapper.DistributionType.BIN
-    }
-
-    named<KotlinCompile>("compileKotlin") {
-        kotlinOptions.jvmTarget = javaVersion.toString()
-    }
-
-    named<JavaCompile>("compileJava") {
-        options.encoding = "UTF-8"
-        options.release.set(javaVersion.toString().toInt())
-    }
-
-    named<Jar>("jar") {
-        from("LICENSE") {
-            rename { "${it}_${base.archivesName.get()}" }
-        }
-    }
-
-    named<Test>("test") {
-        useJUnitPlatform()
-
-        testLogging {
-            outputs.upToDateWhen { false } // When the build task is executed, stderr-stdout of test classes will be show
-            showStandardStreams = true
-        }
-    }
-
 }
---
+++ preconfiguration/readme.md
@@ -0,0 +1,6 @@
+preconfiguration is a folder that contains mods, maps, and 
+others things to use when generating the test server for the first time.
+
+So when you clone the project from GitHub, you don't need to configure your minecraft instance
+with you preferred settings all time, create a test map, download some useful mods for debugging
+and helping you in the development of your mod. Everything is ready
